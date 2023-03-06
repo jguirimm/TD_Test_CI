c777f1bb8124d91c42d4e05bc7dbc6974986a5d0
Chaima Jguirim <Jguirim.Chaima@univ-evry.fr>
2023-03-06 14:06:42 +0100

    Premier git

---
+++ TD_Test_CI.py
@@ -0,0 +1,14 @@
+class CartePizzeria:
+    
+    def is_empty(self):
+        if len(CartePizzeria)==0:
+            return True
+    
+    def nb_pizzas(self):
+        CartePizzeria.count()
+        
+    def add_pizza(pizza):
+        CartePizzeria.add(pizza)
+        
+    def     
+         
